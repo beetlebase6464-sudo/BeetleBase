e0e91263819ce54a5265632a5334be1169c08d24
beetlebase6464-sudo <beetlebase6464@gmail.com>
2026-08-23 22:37:02 +0900

    feat: update top page design and copy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://beetlebase.jp', // 本番ドメインに変更する
-  output: 'hybrid',              // 静的優先 + 必要なルートだけSSR
+  output: 'static',              // 静的優先 + 必要なルートだけSSR
   adapter: cloudflare({
     platformProxy: { enabled: true }, // ローカルdev時にCloudflare環境をエミュレート
   }),
